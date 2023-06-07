af292d2319059b4ef3c620bd9d6684266c7ac8d3
GkhKaya <gkhkaya00@gmail.com>
2023-06-07 20:24:33 +0300

    Created App's Files

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Gokhan Kaya on 7.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
